d1525737ffc6ed8eaf05c29f3fbf5ccd712645fa
Edwin-Alomoto <Edwin_Alomoto@outlook.com>
2023-09-09 17:20:12 -0500

    primera parte de la transccion

--- movimientos.js
+++ movimientos.js
@@ -1,37 +1,31 @@
-movimientos=[
-    {numeroCuenta:"02234567",monto:10.24,tipo:"D"},
-    {numeroCuenta:"02345211",monto:45.90,tipo:"D"},
-    {numeroCuenta:"02234567",monto:65.23,tipo:"C"},
-    {numeroCuenta:"02345211",monto:65.23,tipo:"C"},
-    {numeroCuenta:"02345211",monto:12.0,tipo:"D"},
-]
+movimientos = [
+  { numeroCuenta: "02234567", monto: 10.24, tipo: "D" },
+  { numeroCuenta: "02345211", monto: 45.9, tipo: "D" },
+  { numeroCuenta: "02234567", monto: 65.23, tipo: "C" },
+  { numeroCuenta: "02345211", monto: 65.23, tipo: "C" },
+  { numeroCuenta: "02345211", monto: 12.0, tipo: "D" },
+];
 
-cargar=function(){
-    mostrarComponente("divMovimientos");
-    ocultarComponente("divCuentas");
-    ocultarComponente("divTransacciones");
-    
-}
+cargar = function () {
+  mostrarComponente("divMovimientos");
+  ocultarComponente("divCuentas");
+  ocultarComponente("divTransacciones");
+};
 
-filtrarMovimientos=function(numeroCuenta){
-    let movimientosCuenta=[];
-    //Se barre el arreglo de movimientos
-    //En cada iteración, verifica si el numero de cuenta del movimiento es igual al que recibe como parametro
-    //En caso de serlo, agrega la cuenta al arreglo movimientosCuenta
-    //Invoca a mostrarMovimientos, pasándole como parámetro movimientosCuenta
-}
+filtrarMovimientos = function (numeroCuenta) {
+  //Se barre el arreglo de movimientos
+  //En cada iteración, verifica si el numero de cuenta del movimiento es igual al que recibe como parametro
+  //En caso de serlo, agrega la cuenta al arreglo movimientosCuenta
+  //Invoca a mostrarMovimientos, pasándole como parámetro movimientosCuenta
+};
 
 /*
     Recibe un arreglo con los movimientos que va a mostrar en pantalla
 */
-mostrarMovimientos=function(misMovimientos){
-    //Muestra en pantalla una tabla con los movimientos que recibe en misMovimientos
-    //Columnas: NUMERO CUENTA, MONTO, TIPO
-    //Si ya pinta correctamente la tabla, hacer el siguiente cambio:
-    //Si el tipo es D(DEBITO), mostrar el monto en negativo (multiplicar por -1)
-    //Si el tipo es C(CREDITO), mostrar el monto en positivo (tal como está guardado)
-}
-
-
-
-
+mostrarMovimientos = function (misMovimientos) {
+  //Muestra en pantalla una tabla con los movimientos que recibe en misMovimientos
+  //Columnas: NUMERO CUENTA, MONTO, TIPO
+  //Si ya pinta correctamente la tabla, hacer el siguiente cambio:
+  //Si el tipo es D(DEBITO), mostrar el monto en negativo (multiplicar por -1)
+  //Si el tipo es C(CREDITO), mostrar el monto en positivo (tal como está guardado)
+};
--- transacciones.js
+++ transacciones.js
@@ -1,54 +1,87 @@
-cuentas=[
-    {numeroCuenta:"02234567", cedula:"1714616123",nombre:"Juan",apellido:"Perez",saldo:0.0},
-    {numeroCuenta:"02345211",cedula:"1281238233",nombre:"Felipe",apellido:"Caicedo",saldo:0.0}
-]
+cuentas = [
+  {
+    numeroCuenta: "02234567",
+    cedula: "1714616123",
+    nombre: "Juan",
+    apellido: "Perez",
+    saldo: 0.0,
+  },
+  {
+    numeroCuenta: "02345211",
+    cedula: "1281238233",
+    nombre: "Felipe",
+    apellido: "Caicedo",
+    saldo: 0.0,
+  },
+];
 
-cargar=function(){
-    mostrarComponente("divTransacciones");
-    ocultarComponente("divCuentas");
-    ocultarComponente("divMovimientos");
-    
-}
+cargar = function () {
+  mostrarComponente("divTransacciones");
+  ocultarComponente("divCuentas");
+  ocultarComponente("divMovimientos");
+  deshabilitarComponente("botonDepositar");
+  deshabilitarComponente("botonRetirar");
+};
 
 /*
     Busca la cuenta en el arreglo en función del número de cuenta,
     si existe retorna el objeto cuenta, caso contrario retorna null. 
 */
-buscarCuenta=function(numeroCuenta){
+buscarCuenta = function (numeroCuenta) {
+  let cuentaBuscar;
+  let cuentaEncontrado = null;
+  for (let i = 0; i < cuentas.length; i++) {
+    cuentaBuscar = cuentas[i];
+    if (cuentas[i].numeroCuenta == numeroCuenta) {
+      cuentaEncontrado = cuentaBuscar;
+      break;
+    }
+  }
+  return cuentaEncontrado;
+};
 
-}
+ejecutarBusqueda = function () {
+  let cuentaBusqueda = recuperarTexto("txtBuscarNumeroCuenta");
+  let resultadoBusqueda = buscarCuenta(cuentaBusqueda);
+  if (resultadoBusqueda == null) {
+    alert("CUENTA NO EXISTE");
+  } else {
+    mostrarTexto("txtCuenta", resultadoBusqueda.numeroCuenta);
+    mostrarTexto("txtNombre", resultadoBusqueda.nombre);
+    mostrarTexto("txtSaldo", resultadoBusqueda.saldo);
+    habilitarComponente("botonDepositar");
+    habilitarComponente("botonRetirar");
+  }
+};
 
-ejecutarBusqueda=function(){
-    //toma el numero de cuenta de la caja de texto
-    //invoca a buscarCuenta y guarda el resultado en una variable
-    //Si el resultado es diferente de null, muestra en pantalla, caso contrario muestra un alert
-}
+depositar = function (numeroCuenta, monto) {
+  let cuentaAfectada = buscarCuenta();
+  let cuentaActual = monto + cuentaAfectada.numeroCuenta;
+  return cuentaActual;
+  //invoca a buscarCuenta, guarda el resultado en la variable cuentaAfectada;
+  //Al saldo actual de la cuenta afectada, le suma el monto que recibe como parámetro
+};
 
-depositar=function(numeroCuenta,monto){
-    let cuentaAfectada;
-    //invoca a buscarCuenta, guarda el resultado en la variable cuentaAfectada;
-    //Al saldo actual de la cuenta afectada, le suma el monto que recibe como parámetro
-}
+ejecutarDeposito = function () {
+  let cuentaBusqueda = recuperarTexto("txtBuscarNumeroCuenta");
+  let montoIngresado = parseFloat(recuperarTexto("retxtMonto"));
+  depositar(cuentaBusqueda, montoIngresado);
+  //invoca a depositar
+  //Muestra un mensaje TRANSACCION EXITOSA
+  //Muestra en pantalla el nuevo saldo de la cuenta
+};
 
-ejecutarDeposito=function(){
-    //Toma el numero de cuenta ingresado en la caja de texto
-    //Toma el monto ingresado en la caja de texto
-    //invoca a depositar
-    //Muestra un mensaje TRANSACCION EXITOSA
-    //Muestra en pantalla el nuevo saldo de la cuenta
-}
+depositar = function (numeroCuenta, monto) {
+  let cuentaAfectada;
+  //invoca a buscarCuenta, guarda el resultado en la variable cuentaAfectada;
+  //Al saldo actual de la cuenta afectada, le suma el monto que recibe como parámetro
+};
 
-depositar=function(numeroCuenta,monto){
-    let cuentaAfectada;
-    //invoca a buscarCuenta, guarda el resultado en la variable cuentaAfectada;
-    //Al saldo actual de la cuenta afectada, le suma el monto que recibe como parámetro
-}
-
-retirar=function(numeroCuenta,monto){
-    let cuentaAfectada;
-    //invoca a buscarCuenta, guarda el resultado en la variable cuentaAfectada;
-    //Valida si la cuenta tiene el saldo suficiente para retirar el monto
-    //Si el saldo es suficiente,al saldo actual de la cuenta afectada, le resta el monto que recibe como parámetro
-    //Si el saldo no es suficiente, muestra un alert SALDO INSUFICIENTE
-    //Si logra retirar muestra un mensaje TRANSACCION EXITOSA y muestra en pantalla el nuevo saldo de la cuenta
-}
+retirar = function (numeroCuenta, monto) {
+  let cuentaAfectada;
+  //invoca a buscarCuenta, guarda el resultado en la variable cuentaAfectada;
+  //Valida si la cuenta tiene el saldo suficiente para retirar el monto
+  //Si el saldo es suficiente,al saldo actual de la cuenta afectada, le resta el monto que recibe como parámetro
+  //Si el saldo no es suficiente, muestra un alert SALDO INSUFICIENTE
+  //Si logra retirar muestra un mensaje TRANSACCION EXITOSA y muestra en pantalla el nuevo saldo de la cuenta
+};
